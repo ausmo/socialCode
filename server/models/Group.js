f1704c6a58cee769a4cf43e09ae0b7da650acfe4
Alex Luce <alexluce85@gmail.com>
2022-11-10 13:23:58 -0600

    Post schema good to go

--- server/models/Group.js
+++ server/models/Group.js
@@ -1,47 +1,43 @@
-const { Schema, model } = require('mongoose');
-const dateFormat = require('../utils/dateFormat');
+const { Schema, model } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
 
 const groupSchema = new Schema({
-  
-    group: [
-        {
-           _id: {
-            type: String,
-            required: true,
-            minlength: 3,
-            maxlength: 20
-           },
+  group: [
+    {
+      _id: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 20,
+      },
 
-           groupTitle: {
-            type: String,
-            required: true,
-            minlength: 2,
-            maxlength: 25
-           },
+      groupTitle: {
+        type: String,
+        required: true,
+        minlength: 2,
+        maxlength: 25,
+        trim: true,
+      },
 
-           createdAt: {
-            type: Date,
-            default: Date.now,
-            get: (timestamp) => dateFormat(timestamp),
-           },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (timestamp) => dateFormat(timestamp),
+      },
 
-           groupAuthor: {
-            type: String,
-            required: true
-           },
+      groupAuthor: {
+        type: String,
+        required: true,
+      },
 
-           groupMembers: {
-            type: String, 
-            required: true
-           }
-        }
-
-    ]
+      groupMembers: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
 });
- 
 
-
-
-const Group = model('Group', groupSchema);
+const Group = model("Group", groupSchema);
 
 module.exports = Group;
