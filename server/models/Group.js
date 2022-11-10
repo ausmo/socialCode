f375f091c3b6ecbbfc2fc85ad90ed70d8ea6e0a5
Alex Luce <alexluce85@gmail.com>
2022-11-10 13:05:31 -0600

    Added group schema and thought seeds

---
+++ server/models/Group.js
@@ -0,0 +1,47 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const groupSchema = new Schema({
+  
+    group: [
+        {
+           _id: {
+            type: String,
+            required: true,
+            minlength: 3,
+            maxlength: 20
+           },
+
+           groupTitle: {
+            type: String,
+            required: true,
+            minlength: 2,
+            maxlength: 25
+           },
+
+           createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (timestamp) => dateFormat(timestamp),
+           },
+
+           groupAuthor: {
+            type: String,
+            required: true
+           },
+
+           groupMembers: {
+            type: String, 
+            required: true
+           }
+        }
+
+    ]
+});
+ 
+
+
+
+const Group = model('Group', groupSchema);
+
+module.exports = Group;
