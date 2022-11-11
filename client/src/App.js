717f41433c44ee610f3586a38bc23162e1ca3174
Austin Moore <austin1moore@gmail.com>
2022-11-11 11:29:55 -0600

    debugged login page, ready for react

--- client/src/App.js
+++ client/src/App.js
@@ -8,14 +8,6 @@ import {
 import { setContext } from '@apollo/client/link/context';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import Home from './pages/Home';
-import Signup from './pages/Signup';
-import Login from './pages/Login';
-import SinglePost from './pages/SinglePost';
-import Profile from './pages/Profile';
-import Header from './components/Header';
-import Footer from './components/Footer';
-
 // Construct our main GraphQL API endpoint
 const httpLink = createHttpLink({
   uri: '/graphql',
@@ -44,38 +36,8 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <Router>
-        <div className="flex-column justify-flex-start min-100-vh">
-          <Header />
-          <div className="container">
-            <Routes>
-              <Route 
-                path="/"
-                element={<Home />}
-              />
-              <Route 
-                path="/login"
-                element={<Login />}
-              />
-              <Route 
-                path="/signup"
-                element={<Signup />}
-              />
-              <Route 
-                path="/me"
-                element={<Profile />}
-              />
-              <Route 
-                path="/profiles/:profilename"
-                element={<Profile />}
-              />
-              <Route 
-                path="/posts/:postId"
-                element={<SinglePost />}
-              />
-            </Routes>
-          </div>
-          <Footer />
-        </div>
+      
+  
       </Router>
     </ApolloProvider>
   );
