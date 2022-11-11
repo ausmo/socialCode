3a96b1cb58992e3656d423504067185feb666e1c
Austin Moore <austin1moore@gmail.com>
2022-11-11 13:39:53 -0600

    added bulma to public, frontpage currently login everything else commented out

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// import './index.css';
 import './login.css';
-import './profile.css';
-import './groups.css';
-import './homepage.css';
+// import './profile.css';
+// import './groups.css';
+// import './homepage.css';
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
