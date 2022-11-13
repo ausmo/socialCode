e5fade708da6118765c041d183d75c89b8cd92f5
Alex Luce <alexluce85@gmail.com>
2022-11-13 16:47:30 -0600

    Home2 should be good

--- client/src/pages/Home2.js
+++ client/src/pages/Home2.js
@@ -1,48 +1,28 @@
 import Navbar from "../components/Navbar";
 import PostForm from "../components/PostForm";
-
+import CommentForm from "../components/CommentForm"
+import CommentBody from "../components/CommentBody";
 
 export default function Groups(params) {
-    return(
+  return (
     <>
-    <Navbar/>
-    <h1 class="homepageBanner">socialCode</h1>
-    <br />
-    <br />
-    <br />
-    <br />
-   
- 
-           <PostBody/>
-           <CommentBody/>
-           <CommentForm/>
-           
-       
-        <br />
-        <br />
-       <PostForm/>
-    
-    <br />
-    <br />
-    <article class="homepageMedia">
-      <figure class="media-left">
-        <p class="image is-64x64">
-          <img src="/mocks/img/me2.jpg" alt = "Austin" class="is-rounded"/>
-        </p>
-      </figure>
-      <div class="media-content">
-        <div class="field">
-          <p class="control">
-            <textarea class="textarea" placeholder='console.log("Hello World!")'></textarea>
-          </p>
-        </div>
-        <div class="field">
-          <p class="control">
-            <button class="postButton">Post comment</button>
-          </p>
-        </div>
-      </div>
-    </article>
+      <Navbar />
+      <h1 class="homepageBanner">socialCode</h1>
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <PostBody />
+      <CommentBody />
+      <CommentForm />
+
+      <br />
+      <br />
+      <PostForm />
+
+      <br />
+      <br />
     </>
-    )}
-    
+  );
+}
