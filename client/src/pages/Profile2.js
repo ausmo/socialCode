747b7d291906b161ee714644b48c475c5af99d41
Alex Luce <alexluce85@gmail.com>
2022-11-13 18:21:40 -0600

    converted image and imported, still more to do

--- client/src/pages/Profile2.js
+++ client/src/pages/Profile2.js
@@ -8,6 +8,7 @@ import { useQuery } from "@apollo/client";
 import { QUERY_USER, QUERY_ME } from "../utils/queries";
 import Auth from "../utils/auth";
 import PostBody from "../components/PostBody";
+import savannahjpg from "../img/savannah.jpg"
 
 export default function Home2(params) {
   return (
@@ -25,7 +26,8 @@ export default function Home2(params) {
               {/* All of this needs to change by user, to line 44 */}
               <p class="image is-256x256">
                 <img
-                  src="/mocks/img/me2.jpg"
+                  src="/mocks/img/me2.jpg" 
+                //   need database for profile pic and alt
                   class="is-rounded profilePic"
                   alt="Austin Moore Profile Pic"
                 />
@@ -36,7 +38,8 @@ export default function Home2(params) {
               <br />
               <br />
               <ul class="asl">
-                <li>Dallas, TX</li>
+                {/* need to pull these from sign up database */}
+                <li>Dallas, TX</li> 
                 <li>Frontend Dev</li>
                 <li>Udexx Tech LLC</li>
               </ul>
@@ -76,7 +79,7 @@ export default function Home2(params) {
             <li class="friend">
               <p class="image is-48x48">
                 <img
-                  src="/mocks/img/savannah.jpg"
+                  src= {savannahjpg}
                   class="is-rounded"
                   alt="Savannah McGinnis Profile Pic"
                 />
